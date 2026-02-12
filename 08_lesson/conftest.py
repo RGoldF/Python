23408e1118cdd89a5e49d4c030ff8d039cc7360d
RGoldF <rina.goldfinch@gmail.com>
2026-02-13 03:13:47 +0700

    done

--- 08_lesson/conftest.py
+++ 08_lesson/conftest.py
@@ -1,10 +1,19 @@
 import pytest
-from yougile_api import YougileApi
+from projects_api import ProjectsPage
+
+
+@pytest.fixture(scope="session")
+def projects_page():
+    token = "IsG-1B2xwPXyBbHUwlcyHUUF45AYPFIaYimpaASCOA6f2HDJ-bNs9jFchUkhApeV"
+    base_url = "https://ru.yougile.com/api-v2"
+    return ProjectsPage(base_url, token)
 
 
 @pytest.fixture
-def api() -> YougileApi:
-    token = "IsG-1B2xwPXyBbHUwlcyHUUF45AYPFIaYimpaASCOA6f2HDJ-bNs9jFchUkhApeV"
-    company_id = "62f6932b-a666-4672-b961-d3d3f4295b22"
-    base_url = "https://ru.yougile.com/api-v2"
-    return YougileApi(base_url, token, company_id)
+def temp_project(projects_page):
+    response = projects_page.create("X Project")
+    project_id = response.json()["id"]
+
+    yield project_id
+
+    projects_page.update(project_id, deleted=True)
